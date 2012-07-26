0507f47a8c75732479f632a84c469e90c1ab7d78
Adrian Siekierka <asiekierka@gmail.com>
2012-07-26 19:29:20 +0200

    base player code

--- main.c
+++ main.c
@@ -8,18 +8,23 @@ remember to do it often, or everything will turn to crap!
 
 #include "common.h"
 #include "map.h"
+#include "player.h"
 #include "render.h"
-s32 tx = 0;
-s32 ty = 0;
 
-void display (s32 root_x, s32 root_y)
+player_t *player;
+
+void display (player_t *p)
 {
 	s32 i;
 	s32 j;
-	s32 rx = root_x;
-	s32 ry = root_y;
+	s32 rx = p->x;
+	s32 orx;
+	s32 ry = p->y;
 	tile_t tile;
-	map_layer_set_used_rendered(rx+(SFP_SCREEN_WIDTH/2),ry+(SFP_SCREEN_HEIGHT/2));
+	map_layer_set_used_rendered(rx,ry);
+	rx -= (SFP_SCREEN_WIDTH)/2;
+	ry -= (SFP_SCREEN_HEIGHT)/2;
+	orx = rx;
 	for(j=0;j<SFP_SCREEN_HEIGHT;j++)
 	{
 		for(i=0;i<SFP_SCREEN_WIDTH;i++)
@@ -28,20 +33,30 @@ void display (s32 root_x, s32 root_y)
 			tile = map_get_tile(rx,ry);
 			sfp_putc_block_2x(i,j,(tile.col>>4),(tile.col&15),tile.chr);
 		}
-		rx = root_x;
+		rx = orx;
 		ry++;
 	}
+	// Early player code.
+	sfp_putc_block_2x((SFP_SCREEN_WIDTH)/2,(SFP_SCREEN_HEIGHT)/2,(p->col>>4),(p->col&15),p->chr);
+	char* name = "Gamemaster";
+
+	u32 pnx = (((SFP_SCREEN_WIDTH)/2)*16)-((strlen(name))*4)+8;
+	u32 pny = (((SFP_SCREEN_HEIGHT)/2)*16)-10;
+	sfp_printf_1x(pnx,pny,0x0F,0,"%s",name);
 }
 
 int main(int argc, char *argv[])
 {
 	sfp_init_render();
 
+	player = player_get(PLAYER_SELF);
+
 	sfp_render_begin();
-	display(0,0);
-	sfp_printf_2x(1*8,2*8,0x1F,0,"Hello %s! You are player %i.", "Neo", 42);
-	sfp_printf_1x(3*8,7*8,0x07,0,"(You also suck at this game.)");
+	display(player);
+	sfp_printf_2x(1*8,2*8,0x1F,0,"Hello %s! You are player %i.", "Gamemaster", PLAYER_SELF);
 	sfp_render_end();
+
+	sfp_delay(900);
 
 	return 0;
 }
