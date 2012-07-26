1c39a4d2ac27afd17efedc6ac16631451300e5e5
Adrian Siekierka <asiekierka@gmail.com>
2012-07-26 13:59:28 +0200

    definitions

--- types.h
+++ types.h
@@ -10,4 +10,8 @@ typedef int32_t s32;
 typedef int16_t s16;
 typedef int8_t s8;
 
+typedef struct player { s32 x,y; u16 id; u8 col; u16 char; } player_t;
+typedef struct tile { u8 type; u16 char; u8 col; void *data; } tile_t;
+typedef struct layer { u32 x,y; u16 w,h; tile_t *tiles; } layer_t;
+
 #endif /* _TYPES_H_ */
