5223361608eda8cfa289435720ac2867d33e7c43
Ben Russell (300178622) <thematrixeatsyou@gmail.com>
2012-07-30 22:21:21 +1200

    forgot these two files... also forgot to mention, we now have .gitignore

---
+++ lua.c
@@ -0,0 +1,9 @@
+#include "common.h"
+#include "lua.h"
+#include "types.h"
+
+int sfp_lua_init()
+{
+	// TODO!
+	return 0;
+}
---
+++ lua.h
@@ -0,0 +1,9 @@
+#ifndef _SFP_LUA_H_
+#define _SFP_LUA_H_
+
+#include <lua.h>
+#include <lauxlib.h>
+
+int sfp_lua_init();
+
+#endif /* _SFP_LUA_H_ */
