22631c81818334382c7aca835a7238565405afe6
Adrian Siekierka <asiekierka@gmail.com>
2012-07-31 20:41:13 +0200

    work on physics

--- physics.c
+++ physics.c
@@ -2,35 +2,31 @@
 #include "physics.h"
 #include "types.h"
 
-tile_t* handle_physics_tile(tile_t *origTile, s32 x, s32 y, map_t *map);
 
-void handle_physics(map_t *map, void (*setBlockFunc)(s32, s32, tile_t))
+void handle_physics(map_t *map, void (*setBlockFunc)(map_t *, s32, s32, tile_t *, u32))
 {
 	s32 x, y;
 	tile_t *tile;
-	tile_t *tile2;
+	u32 underCounter;
 
 	// Loopty loop
 	while(map_get_next_update(map,&x,&y))
 	{
 		printf("Got an update on %d,%d\n",x,y);
 		// iterate over all tiles, including underones
-		tile = (tile_t *)malloc(sizeof(tile_t));
+		tile = map_get_tile_ref(map,x,y);
+		underCounter = 0;
+
 		while(tile != NULL && !tile_active(*tile))
 		{
-			tile = (tile_t *)malloc(sizeof(tile_t));
-			*tile = map_get_tile(map,x,y);
+			tile = tile->under;
+			underCounter++;
 		}
+
 		if(tile != NULL)
 		{
-			// handle physics on tile
-			tile2 = handle_physics_tile(tile,x,y,map);
-			if(tile2 != NULL)
-			{
-				(*setBlockFunc)(x,y,*tile);
-				free(tile2);
-			}
-			free(tile);
+			handle_physics_tile(tile,x,y,map);
+			(*setBlockFunc)(map,x,y,tile,underCounter);
 		}
 	}
 }
