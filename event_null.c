e73917ad9beb4e5396a73101258a1a2d274c7502
asiekierka <asiekierka@gmail.com>
2012-11-02 17:13:45 +0100

    added null event impl.

---
+++ event_null.c
@@ -0,0 +1,53 @@
+#include <SDL.h>
+#include "event.h"
+
+void sfp_event_init()
+{
+}
+
+int sfp_event_key(int key)
+{
+	return 0;
+}
+
+void sfp_event_keywait(int key, int time)
+{
+}
+
+int sfp_event_getkeywait(int key)
+{
+	return 0;
+}
+
+void sfp_event_tick()
+{
+}
+
+int sfp_event_getkey()
+{
+	return 0;
+}
+
+int sfp_event_mouse_x()
+{
+	return 0;
+}
+
+int sfp_event_mouse_y()
+{
+	return 0;
+}
+
+int sfp_event_mouse_button(int btn)
+{
+	return 0;
+}
+int sfp_event_mouse_button_press(int btn)
+{
+	return 0;
+}
+
+void sfp_event_poll()
+{
+}
+
