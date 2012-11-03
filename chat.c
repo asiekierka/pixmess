23771963239b67acd3e7765f215ee1102ddd6797
asiekierka <asiekierka@gmail.com>
2012-11-03 12:06:37 +0100

    finished add_chat_msg

--- chat.c
+++ chat.c
@@ -22,4 +22,24 @@ void add_chat_msg(char* msg)
 	
 	// use t just to shut the compiler up
 	printf("time: %lli\n", (long long int)t);
+	if(messages[MAX_CHAT_MSG-1] != NULL)
+	{
+		for(i=1;i<MAX_CHAT_MSG;i++)
+			messages[i-1] = messages[i];
+		messages[MAX_CHAT_MSG-1] = malloc(sizeof(chatmsg_t));
+		messages[MAX_CHAT_MSG-1]->msg = msg;
+		messages[MAX_CHAT_MSG-1]->time = t;
+		return;
+	}
+	else
+	{
+		for(i=0;i<MAX_CHAT_MSG;i++)
+			if(messages[i]==NULL)
+			{
+				messages[i] = malloc(sizeof(chatmsg_t));
+				messages[i]->msg = msg;
+				messages[i]->time = t;
+				return;
+			}
+	}
 }
